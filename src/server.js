75c263853ee8197f53c28adf3ed2ef90b60dd60c
Michael Prather <mprather1@gmail.com>
2017-04-03 15:50:48 +0000

    read from stream

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
-// node streams 
+import fs from 'fs'
+import logger from 'winston-color'
+
+const readableStream = fs.createReadStream("package.json")
+let data = ''
+
+readableStream.on('data', (chunk) => {
+  data+=chunk
+})
+
+readableStream.on('end', () => {
+  const parsed = JSON.parse(data)
+  logger.info('Version:', parsed.version)
+})
